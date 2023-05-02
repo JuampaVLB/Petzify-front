f573fc0a514665c7175473f03579cfd72022586c
JuampaVLB <juampablovillalba25@gmail.com>
2023-05-02 20:14:42 -0300

    added register form with api and routes

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -134,62 +134,6 @@ const LoginScreen = () => {
         </TouchableOpacity>
       </View>
     </View>
-    // <View>
-    //   <Text
-    //     style={{
-    //       alignSelf: "center",
-    //       fontSize: 32,
-    //       marginTop: "40%",
-    //     }}
-    //   >
-    //     Login
-    //   </Text>
-    //   <TouchableOpacity
-    //     onPress={() => navigation.navigate("Home")}
-    //     style={{
-    //       backgroundColor: "purple",
-    //       padding: 10,
-    //       marginTop: "20%",
-    //       width: "50%",
-    //       alignSelf: "center",
-    //       borderRadius: 10,
-    //     }}
-    //   >
-    //     <Text
-    //       style={{
-    //         fontSize: 15,
-    //         textAlign: "center",
-    //         color: "white",
-    //       }}
-    //     >
-    //       Acceder
-    //     </Text>
-    //   </TouchableOpacity>
-
-    //   {/* OTRO BUTTON */}
-
-    //   <TouchableOpacity
-    //     onPress={() => navigation.navigate("Register")}
-    //     style={{
-    //       backgroundColor: "purple",
-    //       padding: 10,
-    //       marginTop: "20%",
-    //       width: "50%",
-    //       alignSelf: "center",
-    //       borderRadius: 10,
-    //     }}
-    //   >
-    //     <Text
-    //       style={{
-    //         fontSize: 15,
-    //         textAlign: "center",
-    //         color: "white",
-    //       }}
-    //     >
-    //       No tengo cuenta.
-    //     </Text>
-    //   </TouchableOpacity>
-    // </View>
   );
 };
 
--- screens/RegisterScreen.jsx
+++ screens/RegisterScreen.jsx
@@ -1,43 +1,128 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  ImageBackground,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Alert,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const RegisterScreen = () => {
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigation = useNavigation();
 
+  const handleSubmit = () => {
+    axios({
+      method: "post",
+      url: "https://pet-tracker-backend-production.up.railway.app/api/v1/auth/signup",
+      data: {
+        username,
+        email,
+        password,
+      },
+    })
+      .then(function (response) {
+        //  let token = response.headers.get("auth-token");
+        Alert.alert(
+          `Register Exitoso! ${response.data.user.username} - ${response.data.user.email}`
+        );
+
+        navigation.navigate("Home");
+      })
+      .catch(function (error) {
+        Alert.alert(error.response.data[0].message);
+      });
+  };
+
   return (
-    <View>
-        <Text
-        style={{
-          alignSelf: "center",
-          fontSize: 32,
-          marginTop: "40%"
-        }}
-        >
-            Register
-        </Text>
+    <View style={styles.image}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre de usuario"
+          defaultValue={username}
+          onChangeText={(newText) => setUsername(newText)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Correo electrónico"
+          defaultValue={email}
+          onChangeText={(newText) => setEmail(newText)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          secureTextEntry={true}
+          defaultValue={password}
+          onChangeText={(newText) => setPassword(newText)}
+        />
         <TouchableOpacity
-        onPress={() => navigation.navigate('Login')}
-        style={{
-          backgroundColor: "purple",
-          padding: 10,
-          marginTop: "20%",
-          width: "50%",
-          alignSelf: "center",
-          borderRadius: 10,
-        }}
+          onPress={() => handleSubmit()}
+          style={{
+            backgroundColor: "purple",
+            padding: 10,
+            marginTop: "20%",
+            width: "50%",
+            alignSelf: "center",
+            borderRadius: 10,
+          }}
         >
           <Text
-          style={{
-            fontSize: 15,
-            textAlign: "center",
-            color: "white",
-          }}
-          >Ya tengo cuenta!</Text>
+            style={{
+              fontSize: 15,
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            Registrarse
+          </Text>
         </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text
+            style={{ color: "midnightblue", textDecorationLine: "underline" }}
+          >
+            Ya tengo cuenta
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default RegisterScreen
+const styles = StyleSheet.create({
+  container: {
+    width: "90%",
+    minHeight: "40%",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+    width: "80%",
+    backgroundColor: "white",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#ccc",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default RegisterScreen;
