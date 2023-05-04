b3eb44db43e7241d1335dc17dd4f197d92a65b22
JuampaVLB <juampablovillalba25@gmail.com>
2023-05-03 23:45:43 -0300

    added api

---
+++ screens/Settings.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const Settings = () => {
+  return (
+    <View>
+        <Text
+        style={{
+          alignSelf: "center",
+          fontSize: 32,
+          marginTop: "40%"
+        }}
+        >
+            Settings
+        </Text>
+    </View>
+  )
+}
+
+export default Settings
