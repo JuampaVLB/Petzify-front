c1e185f43a2fdb116511d0cd5e5cfc3939208136
JuampaVLB <juampablovillalba25@gmail.com>
2023-06-17 01:51:53 -0300

    new modal for added pets on section 'my pet'

--- src/components/ModalPet.jsx
+++ src/components/ModalPet.jsx
@@ -1,7 +1,71 @@
 import React, { useState } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
+
+//
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+//
+
+import SwitchSelector from "react-native-switch-selector";
+import { Dropdown } from "react-native-element-dropdown";
+import * as ImagePicker from "expo-image-picker";
 
 const ModalPet = ({ setEstado, estado }) => {
+  const [isFocus, setIsFocus] = useState(false);
+  const [role, setRole] = useState(null);
+
+  const data = [
+    { label: "Pequeño", value: "user" },
+    { label: "Mediano", value: "business" },
+    { label: "Grande", value: "institution" },
+    { label: "Muy Grande", value: "institution" },
+  ];
+
+  const handleImage = async () => {
+    console.log("clicked image picker");
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      alert("Se requiere permiso para acceder a la biblioteca de medios.");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync();
+
+    if (!result.canceled) {
+      console.log("acepto");
+    }
+
+    // if (!result.canceled) {
+    //   if (Array.isArray(result.assets) && result.assets.length > 0) {
+    //     const selectedAsset = result.assets[0];
+    //     const imageUrl = selectedAsset.uri;
+    //     const fileName = selectedAsset.uri.split("/").pop();
+
+    //     console.log("file name: " + fileName);
+    //     console.log("image url:  " + imageUrl);
+
+    //     try {
+    //       const response = await fetch(result.assets[0].uri);
+    //       const blob = await response.blob();
+
+    //       setFilename1(fileName);
+    //       setBlob1(blob);
+    //     } catch (error) {
+    //       console.log("Error fetching image:", error);
+    //     }
+    //   }
+    // }
+  };
+
   return (
     <View style={styles.centeredView}>
       <Modal
@@ -15,13 +79,111 @@ const ModalPet = ({ setEstado, estado }) => {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setEstado(false)}
-            >
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
+            <Text style={styles.title}>Mi Mascota</Text>
+            <View style={styles.content}>
+              <TextInput
+                style={styles.input}
+                placeholder="Nombre"
+                // defaultValue={title}
+                // onChangeText={(newText) => setTitle(newText)}
+              />
+              <View style={styles.box}>
+                <TextInput
+                  style={styles.input_raza}
+                  placeholder="Raza"
+                  // defaultValue={title}
+                  // onChangeText={(newText) => setTitle(newText)}
+                />
+                <Dropdown
+                  style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+                  placeholderStyle={styles.placeholderStyle}
+                  selectedTextStyle={styles.selectedTextStyle}
+                  inputSearchStyle={styles.inputSearchStyle}
+                  iconStyle={styles.iconStyle}
+                  data={data}
+                  search
+                  maxHeight={300}
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Tamaño"
+                  searchPlaceholder="Buscar..."
+                  value={role}
+                  onFocus={() => setIsFocus(true)}
+                  onBlur={() => setIsFocus(false)}
+                  onChange={(item) => {
+                    setRole(item.value);
+                    setIsFocus(false);
+                  }}
+                />
+              </View>
+              <TouchableOpacity style={styles.btn_img} onPress={handleImage}>
+                <Text
+                  style={{ color: "#000", fontSize: 18, textAlign: "center" }}
+                >
+                  Agregar Imagenes
+                </Text>
+              </TouchableOpacity>
+
+              <View style={styles.label_options}>
+                <Text>Genero</Text>
+                <SwitchSelector
+                  initial={0}
+                  // onPress={(value) => this.setState({ gender: value })}
+                  textColor="#000" //'#7a44cf'
+                  selectedColor="#fff"
+                  buttonColor="green"
+                  borderColor="#ccc"
+                  hasPadding
+                  style={{ width: "100%" }}
+                  options={[
+                    { label: "Feminino", value: "f" },
+                    {
+                      label: "Masculino",
+                      value: "m",
+                    },
+                  ]}
+                  testID="gender-switch-selector"
+                  accessibilityLabel="gender-switch-selector"
+                />
+              </View>
+              <View style={styles.label_options}>
+                <Text>Collar</Text>
+                <SwitchSelector
+                  initial={0}
+                  // onPress={(value) => this.setState({ gender: value })}
+                  textColor="#000" //'#7a44cf'
+                  selectedColor="#fff"
+                  buttonColor="green"
+                  borderColor="#ccc"
+                  hasPadding
+                  style={{ width: "100%" }}
+                  options={[
+                    { label: "No", value: "false" },
+                    {
+                      label: "Si",
+                      value: "true",
+                    },
+                  ]}
+                  testID="gender-switch-selector"
+                  accessibilityLabel="gender-switch-selector"
+                />
+              </View>
+              <View style={styles.box_end}>
+                <TouchableOpacity
+                  style={styles.btn_cancel}
+                  onPress={() => setEstado(false)}
+                >
+                  <Text style={{ color: "#000", textAlign: "center" }}>
+                    Cancelar
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btn_submit}>
+                  <Text style={{ color: "#fff", textAlign: "center" }}>
+                    Agregar Mascota
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
         </View>
       </Modal>
@@ -34,14 +196,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22,
+    backgroundColor: "#000000AA",
   },
   modalView: {
-    margin: 20,
-    backgroundColor: "white",
+    width: "95%",
+    height: "95%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    marginBottom: 20,
     borderRadius: 20,
     padding: 35,
-    alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -71,6 +237,124 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     textAlign: "center",
   },
+  input: {
+    width: 330,
+    height: 50,
+    backgroundColor: "white",
+    paddingLeft: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.3,
+    elevation: 4,
+  },
+  input_raza: {
+    width: "55%",
+    height: 50,
+    backgroundColor: "white",
+    paddingLeft: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.3,
+    elevation: 4,
+  },
+  label_options: {
+    width: "100%",
+    display: "flex",
+  },
+  content: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  btn_img: {
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+    height: 50,
+    backgroundColor: "white",
+    paddingLeft: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.3,
+    elevation: 4,
+    borderRadius: 15,
+  },
+  dropdown: {
+    height: 50,
+    width: "40%",
+    borderColor: "black",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    backgroundColor: "white",
+  },
+  icon: {
+    marginRight: 5,
+    color: "green",
+  },
+  label: {
+    position: "absolute",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    display: "none",
+  },
+  box: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  box_end: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  btn_submit: {
+    backgroundColor: "green",
+    width: "60%",
+    padding: 10,
+    borderRadius: 15,
+  },
+  btn_cancel: {
+    width: "35%",
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 15,
+  },
+  title: {
+    fontSize: 22,
+  }
 });
 
 export default ModalPet;
--- src/screens/drawer/MyPet.jsx
+++ src/screens/drawer/MyPet.jsx
@@ -1,37 +1,34 @@
 import React, { useState } from "react";
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-} from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text, alert } from "react-native";
 
 import ModalPet from "../../components/ModalPet";
 
 const MyPet = () => {
-
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <View style={styles.container}>
-        <Text
+      <Text
         style={{
           alignSelf: "center",
           fontSize: 32,
-          marginTop: "40%"
+          marginTop: "40%",
         }}
-        >
-            My Pet
-        </Text>
-        <TouchableOpacity style={styles.add}>
-          <Text style={{fontSize: 32, color: "white"}}>
-            +
-          </Text>
-        </TouchableOpacity>
+      >
+        My Pet
+      </Text>
+      <TouchableOpacity
+        style={styles.add}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={{ fontSize: 32, color: "white" }}>+</Text>
+      </TouchableOpacity>
+      <View>
         <ModalPet setEstado={setModalVisible} estado={modalVisible} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -46,8 +43,8 @@ const styles = StyleSheet.create({
     margin: 15,
     padding: 20,
     borderRadius: 10,
-    backgroundColor: "red",
-  }
+    backgroundColor: "green",
+  },
 });
 
 export default MyPet;
